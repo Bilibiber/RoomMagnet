6ec3484c3e3572a794d7a6e084127f9b715dce2c
Trent Howard <howardtx@dukes.jmu.edu>
2019-10-21 20:27:55 -0400

    added property and property room classes

---
+++ RoomMagnet/RoomMagnet/App_Code/Properties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Property
+/// </summary>
+public class Property
+{
+    public Property()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
