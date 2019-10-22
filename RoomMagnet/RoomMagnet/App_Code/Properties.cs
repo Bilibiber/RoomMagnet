77435bf8091da36f772281e094319ce7a6c56d3c
Trent Howard <howardtx@dukes.jmu.edu>
2019-10-21 20:49:46 -0400

    changes to properties class

--- RoomMagnet/RoomMagnet/App_Code/Properties.cs
+++ RoomMagnet/RoomMagnet/App_Code/Properties.cs
@@ -3,15 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-/// <summary>
-/// Summary description for Property
-/// </summary>
+
 public class Property
 {
-    public Property()
+    private int PropertyID;
+    private String StreetAddress;
+    private String City;
+    private String HomeState;
+    private String Country;
+    private String ZipCode;
+    private String Filters;
+    private String ImagePath;
+    private DateTime LastUpdated;
+    private String LastUpdatedBy;
+    private int HostID;
+    public Property(int PropertyID, String StreetAddress, String City, String HomeState,
+        String Country, String ZipCode, String Filters, String ImagePath, DateTime LastUpdated,
+        String LastUpdatedBy, int HostID)
     {
-        //
-        // TODO: Add constructor logic here
-        //
+        
     }
+    public void setPropertyID(int PropertyID)
+    {
+        this.PropertyID = PropertyID;
+    }
+    public void setStreetAddress(String StreetAddress)
+    {
+        this.StreetAddress = StreetAddress;
+    }
+    public void setCity(String City)
+    {
+        this.City = City;
+    }
+    public void setCountry(String Country)
+    {
+        this.Country = Country;
+    }
+    public void setZipCode(String ZipCode)
+    {
+        this.ZipCode = ZipCode;
+    }
+    public void setFilters(String Filters)
+    {
+        this.Filters = Filters;
+    }
+    public void setImagePath(String ImagePath)
+    {
+        this.ImagePath = ImagePath;
+    }
+    
 }
