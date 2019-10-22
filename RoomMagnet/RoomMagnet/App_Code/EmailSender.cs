ace546a53b8685182145c46e61170d7168aee571
FanGuo484 <49997239+FanGuo484@users.noreply.github.com>
2019-10-21 20:59:06 -0400

    EmailSender class created

---
+++ RoomMagnet/RoomMagnet/App_Code/EmailSender.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Mail;
+/// <summary>
+/// Summary description for Email
+/// </summary>
+public class Email
+{
+    public const string FromEmailAddress = "roommagnetofficial@gmail.com";
+    public const string FromEmailPassword = "@a1S2d3F4";
+    public string ToEmailAddress;
+    public const string GmailSmtp = "smtp.gmail.com";
+    public const int GmailSmtpPort = 587;
+    public Email(string EmailAddress)
+    {
+        this.ToEmailAddress = EmailAddress;
+    }
+    public void SendWelcomeMail(string EnteredEmailAddress)
+    {
+        MailMessage mail = new MailMessage();
+        mail.From = new MailAddress(FromEmailAddress);
+        mail.To.Add(EnteredEmailAddress);
+        mail.Subject = "Welcome to RoomMagnet";
+        mail.Body = "Testing Email function";
+
+        SmtpClient smtpClient = new SmtpClient(GmailSmtp, GmailSmtpPort);
+        smtpClient.Credentials = new System.Net.NetworkCredential(FromEmailAddress, FromEmailPassword);
+        smtpClient.EnableSsl = true;
+        smtpClient.Send(mail);
+    }
+}
+
