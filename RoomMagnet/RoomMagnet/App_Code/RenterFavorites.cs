de19b46fc0fc69a77bbfea3ceb7a4534b85856d6
ccole31 <55515981+ccole31@users.noreply.github.com>
2019-10-21 21:16:23 -0400

    ADD PropertySearch/RenterFavorites Classes with Data Fields, Getters, and Setters

---
+++ RoomMagnet/RoomMagnet/App_Code/RenterFavorites.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for RenterFavorites
+/// </summary>
+public class RenterFavorites
+{
+    private int RenterID;
+    private int FavoriteProperties;
+
+    public RenterFavorites()
+    {
+
+    }
+
+    public void setRenterID(int RenterID)
+    {
+        this.RenterID = RenterID;
+    }
+
+    public void setFavoriteProperties(int FavoriteProperties)
+    {
+        this.FavoriteProperties = FavoriteProperties;
+    }
+
+    public int getRenterID()
+    {
+        return this.RenterID;
+    }
+
+    public int getFavoriteProperties()
+    {
+        return this.FavoriteProperties;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
