c7dccaf509cd21534b2d03bf35cc91e8e5f36eb1
Mason <labpatron@SHO206-1500-COB>
2019-11-04 16:40:10 -0500

    test

---
+++ RoomMagnet/RoomMagnet/App_Code/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for StartUp
+/// </summary>
+public class StartUp
+{
+    public StartUp()
+    {
+        var builder = new IConfigurationBuilder()
+     .SetBasePath(env.ContentRootPath)
+     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+     .AddEnvironmentVariables();
+        Configuration = builder.Build();
+    }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add framework services.
+        services.AddMvc();
+
+        services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
+        services.AddAWSService<IAmazonS3>();
+        services.AddAWSService<IAmazonDynamoDB>();
+    }
+}
