ae90a79b294ee11c1dab39583ea7af5e96ef8a31
labpatron <labpatron@SHO206-1501-COB>
2019-11-04 23:21:13 -0500

    update addproperty

--- RoomMagnet/RoomMagnet/WebPages/AddProperty.aspx.cs
+++ RoomMagnet/RoomMagnet/WebPages/AddProperty.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.IO;
 
 public partial class WebPages_AddProperty : System.Web.UI.Page
 {
@@ -113,5 +114,81 @@ public partial class WebPages_AddProperty : System.Web.UI.Page
     protected void testimage_Click(object sender, EventArgs e)
     {
         
+    }
+    //public string SaveFile(HttpPostedFile file)
+    //{
+    //    // Specify the path to save the uploaded file to.
+    //    string savePath = "c:\\temp\\uploads\\";
+
+    //    // Get the name of the file to upload.
+    //    //string fileName = FileUpload1.FileName;
+
+    //    // Create the path and file name to check for duplicates.
+    //    string pathToCheck = savePath + fileName;
+
+    //    // Create a temporary file name to use for checking duplicates.
+    //    string tempfileName = "";
+
+    //    // Check to see if a file already exists with the
+    //    // same name as the file to upload.        
+    //    if (System.IO.File.Exists(pathToCheck))
+    //    {
+    //        int counter = 2;
+    //        while (System.IO.File.Exists(pathToCheck))
+    //        {
+    //            // if a file with this name already exists,
+    //            // prefix the filename with a number.
+    //            tempfileName = counter.ToString() + fileName;
+    //            pathToCheck = savePath + tempfileName;
+    //            counter++;
+    //        }
+
+    //        fileName = tempfileName;
+
+    //        // Notify the user that the file name was changed.
+    //        Label4.Text = "A file with the same name already exists." +
+    //            "<br />Your file was saved as " + fileName;
+    //    }
+    //    else
+    //    {
+    //        // Notify the user that the file was saved successfully.
+    //        Label4.Text = "Your file was uploaded successfully.";
+    //    }
+    //    // Append the name of the file to upload to the path.
+    //   // savePath += fileName;
+
+    //    // Call the SaveAs method to save the uploaded
+    //    // file to the specified directory.
+    //    //FileUpload1.SaveAs(savePath);
+    //    return savePath;
+    //}
+
+    
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string filepath = Server.MapPath("\\Upload");
+        HttpFileCollection uploadedFiles = Request.Files;
+        string fileNames = String.Empty;
+        string filePaths = String.Empty;
+
+        for (int i = 0; i < uploadedFiles.Count; i++)
+        {
+            HttpPostedFile userPostedFile = uploadedFiles[i];
+            try
+            {
+                if (userPostedFile.ContentLength > 0)
+                {
+                    userPostedFile.SaveAs(filepath + "\\" + Path.GetFileName(userPostedFile.FileName));
+                    fileNames += userPostedFile.FileName + " ";
+                    filePaths += filepath + "\\" + Path.GetFileName(userPostedFile.FileName) + " ";
+                }
+            }
+            catch (Exception)
+            {
+                Label4.Text = "An unexpected error has occurred";
+            }
+        }
     }
 }
