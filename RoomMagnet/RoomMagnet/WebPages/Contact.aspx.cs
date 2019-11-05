2784e48477261fdf31372265c0052da7b3d4814a
labpatron <labpatron@SHO206-1494-COB>
2019-11-04 21:22:24 -0500

    contact updates

--- RoomMagnet/RoomMagnet/WebPages/Contact.aspx.cs
+++ RoomMagnet/RoomMagnet/WebPages/Contact.aspx.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Net.Mail;
 using System.Windows.Forms;
 
 
