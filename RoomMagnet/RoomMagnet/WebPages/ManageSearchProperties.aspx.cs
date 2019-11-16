78f99adfa7e1e8748e0139ff2be68e1b9ada4a37
ccole31 <55515981+ccole31@users.noreply.github.com>
2019-11-16 15:16:57 -0500

    update manageSearchProperties

--- RoomMagnet/RoomMagnet/WebPages/ManageSearchProperties.aspx.cs
+++ RoomMagnet/RoomMagnet/WebPages/ManageSearchProperties.aspx.cs
@@ -33,18 +33,18 @@ public partial class WebPages_ManageSearchProperties : System.Web.UI.Page
     "[Rating].PropertyID WHERE ([Property].PropertyID = " + Session["ResultPropertyID"] + ")";
 
 
-        
+
         connection.Open();
         string title = "";
         string city = "";
         string homeState = "";
         string zipCode = "";
-        int AvailableBedrooms = -1;
+        int availableBedrooms = -1;
         string rentPrice = "";
         DateTime startDate = DateTime.Now;
         DateTime endDate = DateTime.Now;
         int availableBathrooms = -1;
-        string airConditioning="";
+        string airConditioning = "";
         string heating = "";
         string onSiteLaundry = "";
         string parking = "";
@@ -60,7 +60,7 @@ public partial class WebPages_ManageSearchProperties : System.Web.UI.Page
         decimal numStars = 0;
         DateTime lastUpdated = DateTime.Now;
         string lastUpdatedBy = "";
-      
+
 
         SqlCommand search = new SqlCommand(sql, connection);
         SqlDataReader reader = search.ExecuteReader();
@@ -69,196 +69,196 @@ public partial class WebPages_ManageSearchProperties : System.Web.UI.Page
         int counter = 0;
         if (reader.HasRows)
         {
-            while (reader.Read())
+            reader.Read();
+            byte[] propertyImage = (byte[])reader[8];
+
+            string propertyImageURL = "data:image;base64," + Convert.ToBase64String(propertyImage);
+            if (tempImages.Contains(propertyImageURL) == false)
             {
-
-                byte[] propertyImage = (byte[])reader[8];
-
-
-
-
-                string propertyImageURL = "data:image;base64," + Convert.ToBase64String(propertyImage);
-                if (tempImages.Contains(propertyImageURL) == false)
-                {
-                    tempImages.Add(propertyImageURL);
-
-                    if (counter == 0)
-                    {
-                        if (tempImages.Contains(propertyImage1.ImageUrl) == false)
-                        {
-
-
-                            propertyImage1.ImageUrl = propertyImageURL;
-                            propertyImage1.Visible = true;
-                        }
-                    }
-
-                    else if (counter == 1)
-                    {
-                        if (tempImages.Contains(propertyImage2.ImageUrl) == false)
-                        {
-
-
-                            propertyImage2.ImageUrl = propertyImageURL;
-                            propertyImage2.Visible = true;
-                        }
-                    }
-
-                    else if (counter == 2)
-                    {
-                        if (tempImages.Contains(propertyImage3.ImageUrl) == false)
-                        {
-
-
-                            propertyImage3.ImageUrl = propertyImageURL;
-                            propertyImage3.Visible = true;
-                        }
-                    }
-                    else if (counter == 3)
-                    {
-                        if (tempImages.Contains(propertyImage4.ImageUrl) == false)
-                        {
-
-
-                            propertyImage4.ImageUrl = propertyImageURL;
-                            propertyImage4.Visible = true;
-                        }
-                    }
-                }
+                tempImages.Add(propertyImageURL);
 
                 if (counter == 0)
                 {
-                    Title = reader.GetString(0);
-                    city = reader.GetString(1);
-                    homeState = reader.GetString(2);
-                    zipCode = reader.GetString(3);
-                    AvailableBedrooms = reader.GetInt32(4);
-                    decimal x = reader.GetDecimal(5);
-                    rentPrice = String.Format("{0:0.##}", x);
-                    RentPriceTxtBx.Text = "$" + rentPrice + "/Month";
-                    startDate = reader.GetDateTime(6);
-                    endDate = reader.GetDateTime(7);
-
-                    availableBathrooms = reader.GetInt32(9);
-                    airConditioning = reader.GetString(10);
-                    heating = reader.GetString(11);
-                    onSiteLaundry = reader.GetString(12);
-                    parking = reader.GetString(13);
-                    furnished = reader.GetString(14);
-                    petFriendly = reader.GetString(15);
-                    carbonMonoxideDetector = reader.GetString(16);
-                    smokeDetector = reader.GetString(17);
-                    separateEntrance = reader.GetString(18);
-                    wifi = reader.GetString(19);
-                    tv = reader.GetString(20);
-                    seperateBathroom = reader.GetString(21);
-                    if (reader.IsDBNull(22) == false)
+                    if (tempImages.Contains(propertyImage1.ImageUrl) == false)
                     {
-                        descriptions = reader.GetString(22);
+
+
+                        propertyImage1.ImageUrl = propertyImageURL;
+                        propertyImage1.Visible = true;
                     }
-                    numStars = reader.GetDecimal(23);
-                    lastUpdated = reader.GetDateTime(24);
-                    lastUpdatedBy = reader.GetString(25);
-                   
                 }
 
-                amenitiesLbl.Visible = true;
-
-                if (airConditioning == "Y")
+                else if (counter == 1)
+                {
+                    if (tempImages.Contains(propertyImage2.ImageUrl) == false)
                     {
-                        airConditioningLbl.Text = "Air Conditioning";
-                        airConditioningLbl.Visible = true;
 
+
+                        propertyImage2.ImageUrl = propertyImageURL;
+                        propertyImage2.Visible = true;
+                    }
                 }
 
-                if (heating == "Y")
+                else if (counter == 2)
                 {
-                    heatingLbl.Text = "Heating";
-                    heatingLbl.Visible = true;
-                }
+                    if (tempImages.Contains(propertyImage3.ImageUrl) == false)
+                    {
 
-                if (onSiteLaundry == "Y")
+
+                        propertyImage3.ImageUrl = propertyImageURL;
+                        propertyImage3.Visible = true;
+                    }
+                }
+                else if (counter == 3)
                 {
-                    onSiteLaundryLbl.Text = "On Site Laundry";
-                    onSiteLaundryLbl.Visible = true;
+                    if (tempImages.Contains(propertyImage4.ImageUrl) == false)
+                    {
 
+
+                        propertyImage4.ImageUrl = propertyImageURL;
+                        propertyImage4.Visible = true;
+                    }
                 }
-
-                if (parking == "Y")
-                {
-                    parkingLbl.Text = "Parking";
-                    parkingLbl.Visible = true;
-
-                }
-
-                if (furnished == "Y")
-                {
-                    furnishedLbl.Text = "Furnished";
-                    furnishedLbl.Visible = true;
-
-                }
-
-                if (petFriendly == "Y")
-                {
-                    petFriendlyLbl.Text = "Pet Friendly";
-                    petFriendlyLbl.Visible = true;
-
-                }
-
-                if (carbonMonoxideDetector == "Y")
-                {
-                    carbonMonoxideDetectorLbl.Text = "Carbon Monoxide Detector";
-                    carbonMonoxideDetectorLbl.Visible = true;
-
-                }
-
-                if (smokeDetector == "Y")
-                {
-                    smokeDetectorLbl.Text = "Smoke Detector";
-                    smokeDetectorLbl.Visible = true;
-
-                }
-
-                if (separateEntrance == "Y")
-                {
-                    separateEntranceLbl.Text = "Seperate Entrance";
-                    separateEntranceLbl.Visible = true;
-
-                }
-
-                if (wifi == "Y")
-                {
-                    wifiLbl.Text = "Wifi";
-                    wifiLbl.Visible = true;
-
-                }
-
-                if (tv == "Y")
-                {
-                   tvLbl.Text = "TV";
-                    tvLbl.Visible = true;
-
-                }
-
-                if (seperateBathroom == "Y")
-                {
-                    seperateBathroomLbl.Text = "Seperate Bathroom";
-                    seperateBathroomLbl.Visible = true;
-
-                }
-
-
-
-                string StartDate = startDateLbl.Text;
-                string EndDate = endDateLbl.Text;
-                string AvailableBathrooms = availableBathroomsLbl.Text;
-                string Descriptions = descriptionsLbl.Text;
-                string NumStars = numStarsLbl.Text;
-                string LastUpdated = lastUpdatedLbl.Text;
-                string Rating = ratingLbl.Text;
-                counter++;
             }
+
+            if (counter == 0)
+            {
+                Title = reader.GetString(0);
+                city = reader.GetString(1);
+                homeState = reader.GetString(2);
+                zipCode = reader.GetString(3);
+                availableBedrooms = reader.GetInt32(4);
+                decimal x = reader.GetDecimal(5);
+                rentPrice = String.Format("{0:0.##}", x);
+                rentPriceLbl.Text = "$" + rentPrice + "/Month";
+
+                startDate = reader.GetDateTime(6);
+                endDate = reader.GetDateTime(7);
+
+                //string title1 = (string)reader["Title"];
+
+                availableBathrooms = reader.GetInt32(9);
+                airConditioning = reader.GetString(10);
+                heating = reader.GetString(11);
+                onSiteLaundry = reader.GetString(12);
+                parking = reader.GetString(13);
+                furnished = reader.GetString(14);
+                petFriendly = reader.GetString(15);
+                carbonMonoxideDetector = reader.GetString(16);
+                smokeDetector = reader.GetString(17);
+                separateEntrance = reader.GetString(18);
+                wifi = reader.GetString(19);
+                tv = reader.GetString(20);
+                seperateBathroom = reader.GetString(21);
+                if (reader.IsDBNull(22) == false)
+                {
+                    descriptions = reader.GetString(22);
+                }
+                numStars = reader.GetDecimal(23);
+                lastUpdated = reader.GetDateTime(24);
+                lastUpdatedBy = reader.GetString(25);
+
+            }
+
+
+
+
+            amenitiesLbl.Visible = true;
+
+            if (airConditioning == "Y")
+            {
+                airConditioningLbl.Text = "Air Conditioning";
+                airConditioningLbl.Visible = true;
+
+            }
+
+            if (heating == "Y")
+            {
+                heatingLbl.Text = "Heating";
+                heatingLbl.Visible = true;
+            }
+
+            if (onSiteLaundry == "Y")
+            {
+                onSiteLaundryLbl.Text = "On Site Laundry";
+                onSiteLaundryLbl.Visible = true;
+
+            }
+
+            if (parking == "Y")
+            {
+                parkingLbl.Text = "Parking";
+                parkingLbl.Visible = true;
+
+            }
+
+            if (furnished == "Y")
+            {
+                furnishedLbl.Text = "Furnished";
+                furnishedLbl.Visible = true;
+
+            }
+
+            if (petFriendly == "Y")
+            {
+                petFriendlyLbl.Text = "Pet Friendly";
+                petFriendlyLbl.Visible = true;
+
+            }
+
+            if (carbonMonoxideDetector == "Y")
+            {
+                carbonMonoxideDetectorLbl.Text = "Carbon Monoxide Detector";
+                carbonMonoxideDetectorLbl.Visible = true;
+
+            }
+
+            if (smokeDetector == "Y")
+            {
+                smokeDetectorLbl.Text = "Smoke Detector";
+                smokeDetectorLbl.Visible = true;
+
+            }
+
+            if (separateEntrance == "Y")
+            {
+                separateEntranceLbl.Text = "Seperate Entrance";
+                separateEntranceLbl.Visible = true;
+
+            }
+
+            if (wifi == "Y")
+            {
+                wifiLbl.Text = "Wifi";
+                wifiLbl.Visible = true;
+
+            }
+
+            if (tv == "Y")
+            {
+                tvLbl.Text = "TV";
+                tvLbl.Visible = true;
+
+            }
+
+            if (seperateBathroom == "Y")
+            {
+                seperateBathroomLbl.Text = "Seperate Bathroom";
+                seperateBathroomLbl.Visible = true;
+
+            }
+
+            startDateLbl.Text = startDate.ToString();
+            endDateLbl.Text = endDate.ToString();
+            availableBathroomsLbl.Text = availableBathrooms.ToString();
+            descriptionsLbl.Text = descriptions.ToString();
+            numStarsLbl.Text = numStars.ToString();
+            lastUpdatedLbl.Text = lastUpdated.ToString();
+
             
+            counter++;
+
+
             connection.Close();
             tempImages.Clear();
         }
