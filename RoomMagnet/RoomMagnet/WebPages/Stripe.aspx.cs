7dc33ddc13ed4cd76e6677549f43eda5a5a3f658
Mason <labpatron@BUR251-1876-COB>
2019-11-17 17:31:21 -0500

    stripe test

---
+++ RoomMagnet/RoomMagnet/WebPages/Stripe.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Stripe;
+
+public partial class WebPages_Stripe : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
